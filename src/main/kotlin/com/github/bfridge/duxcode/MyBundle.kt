b4566f1c315a22c4c4b171fe460b1aba9942845b
GitHub Action <action@github.com>
2021-04-09 14:48:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/bfridge/duxcode/MyBundle.kt
+++ src/main/kotlin/com/github/bfridge/duxcode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bfridge.duxcode
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bfridge/duxcode/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bfridge/duxcode/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bfridge.duxcode.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bfridge.duxcode.MyBundle
 
 class MyApplicationService {
 
